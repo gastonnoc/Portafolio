a7eafbdc59a4a0a0a80326ead61428620aecc9d3
gastonnoc <gaston.nocquet.sonido@gmail.com>
2026-02-13 17:54:23 -0300

    Aabout me actualization

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,10 +9,9 @@ const AboutMe = () => {
       
       <p className="text">
         I’m a Full Stack MERN developer with a strong passion for building
-        efficient and user-friendly web applications. I graduated this year from
+        efficient and user-friendly web applications. I graduated from
         UTN (Universidad tecnológica nacional / National Technological
-        University), but my journey in web development began around two years
-        ago through self-directed learning. For the past year, I’ve been
+        University), but my journey in web development began before through self-directed learning. I’ve been
         applying these skills in my current job, where I’ve developed custom web
         tools that helped solve real operational challenges and significantly
         improved day-to-day workflows. I’m always looking for new ways to
